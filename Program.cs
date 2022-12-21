8b41b1c924f90cf917c661cd8cc6d1f3db3a669a
ronitshinde <rony.shinde753@gmail.com>
2022-12-21 18:00:55 +0530

    Hello Logical Programs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Logicalprograms
+{
+
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Logic!!!");
+            //PerfectNumber.PerfectNum();
+            //ReverseNumber.Reversenum();
+            //FibonanciSeries.DisplayFibonanciSeries();
+            //PrimeNumber.PrimeN();
+        }
+    }
+}
